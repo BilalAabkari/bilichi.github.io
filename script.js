a1deac73228a81c7f00350365907949816452058
bilichi123 <102427069+bilichi123@users.noreply.github.com>
2022-11-07 12:00:29 +0100

    added a button to show the left section for the mobile version and improve the responsiveness of the website

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var currentSlide;
+menu_is_open = false;
 
 function displayGallery() {
     var images = document.getElementsByClassName("slideElement");
@@ -39,4 +40,47 @@ function nextSlide() {
     }
     images[currentSlide].style.display = "inline";
 
+}
+
+
+function process_menu_click() {
+    const element = document.getElementById("leftSection");
+    if (!menu_is_open) {
+        let offset = 0; //This is the value that will assign to max-width progressively for the animation
+        let fontSize  = 0; //This is the value that will assign to font-size progressively for the animation
+        let timer = setInterval( function() {
+            if (offset == 80 && fontSize == 80) { //I choose the same number to make a simpler code, and it looks fine
+                clearInterval(timer); //If we reached 80% of max width and 80% of font-size, stop the animation.
+            }
+            else { 
+                offset = offset + 1;
+                element.style.maxWidth = offset + '%';
+                fontSize = fontSize + 1;
+                element.style.fontSize = fontSize + '%';
+            }
+        }, 5);
+    }
+    else {
+        let offset = 80;
+        let fontSize  = 80;
+        let timer = setInterval( function() {
+            if (offset == 0 && fontSize == 0) {
+                clearInterval(timer);
+            }
+            else {
+                offset = offset - 1;
+                element.style.maxWidth = offset + '%';
+                fontSize = fontSize - 2;
+                element.style.fontSize = fontSize + '%';
+            }
+        }, 5);
+    }
+    menu_is_open = !menu_is_open;
+    setTimeout(null, 1000);    
+}
+
+window.onresize = function(){ //This function resets the values to default if we change the size of the window to avoid having to refresh
+    const element = document.getElementById("leftSection");
+    element.style.maxWidth = "";
+    element.style.fontSize = "";
 }
